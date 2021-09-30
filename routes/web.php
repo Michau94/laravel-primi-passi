3578760c6ca41ab512656ddd1cb6d74bf263d3ea
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-09-30 13:51:49 +0200

    added home blade and dynamic data

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $data = [
+        'name' => 'Michau',
+        'lastname' => 'Wado'
+    ];
+    return view('home', $data);
+})->name('home');
